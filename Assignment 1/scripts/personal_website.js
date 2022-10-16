aafdb9722d9b538d52bf676ab1edada17d6fd6a7
twang72 <71084257+twang72@users.noreply.github.com>
2022-10-16 00:04:25 -0700

    Readjusted the theme-related functions and saved the current them to localstorage.

--- Assignment 1/scripts/personal_website.js
+++ Assignment 1/scripts/personal_website.js
@@ -1,17 +1,35 @@
-function themeFunction(){
-    var element = document.body;
-    element.classList.toggle("dark");
-    changeThemeText()
-}
-
-function changeThemeText(){
-    var button = document.getElementById("themebutton");
-    if (button.innerText === 'DARK'){
-        button.innerText = 'Light';
+//Theme Change
+const btn = document.querySelector('.themebutton');
+const mainBody = document.querySelector('body');
+let themeLight = btn.innerText;
+if(localStorage.getItem('theme')){
+    if(localStorage.getItem('theme') === 'Light'){
+        themeFunction();
     } else {
-        button.innerText = 'Dark';
+        localStorage.setItem('theme', themeLight);
+    }
+    btn.addEventListener('click', themeFunction);
+    
+    function themeFunction(){
+        if (themeLight === 'Dark'){
+            themeLight = 'Light';
+            btn.innerText = 'Light';
+        } else {
+            themeLight = 'Dark';
+            btn.innerText = 'Dark';
+        }
+        localStorage.setItem('theme', themeLight);
+        document.body.classList.toggle('dark');
     }
 }
+
+
+
+
+
+
+
+
 
 
 
