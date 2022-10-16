2eca5f4d32eeffc01367f7659a09f238837368e0
twang72 <71084257+twang72@users.noreply.github.com>
2022-10-16 01:27:12 -0700

    Created the functions in js to properly display time and date.

--- Assignment 1/scripts/personal_website.js
+++ Assignment 1/scripts/personal_website.js
@@ -1,4 +1,4 @@
-//Theme Change
+//Theme Change Functions
 const btn = document.querySelector('.themebutton');
 const mainBody = document.querySelector('body');
 let themeLight = btn.innerText;
@@ -23,7 +23,50 @@ if(localStorage.getItem('theme')){
     }
 }
 
+//Date and Time Functions
+const timeDisplay = document.querySelector('.date-time');
+const month_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const day_names = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
 
+timeDisplay.addEventListener('submit', function(event){
+    const timeDisplayData = new FormData(timeDisplay);
+    const myDate = new Date();
+    let text = "";
+    for(const entry of timeDisplayData){
+        if(entry[1] === 'date') {
+            const day = day_names[myDate.getDay()];
+            const month = month_names[myDate.getMonth()];
+            const date = myDate.getDate();
+            const year = myDate.getFullYear();
+            text = [day, month, date, year].join(" ");
+        } else {
+            let dayPeriod = 'AM';
+            let hour = myDate.getHours();
+            const minute = myDate.getMinutes();
+            const second = myDate.getSeconds();
+            if (hour === 0) {
+                hour = 12;
+            } else if (hour === 12) {
+                dayPeriod = 'PM';
+            } else if (hour > 12) {
+                hour = hour % 12;
+                dayPeriod = 'PM';
+            }
+            text = [hour, minute, second].join(':') + " " + dayPeriod;
+        }
+    }
+    console.log(text);
+    const timeText = document.querySelector('#time-text');
+    if (timeText) {
+        timeText.innerText = text;
+    } else {
+        const newTimeText = document.createElement('p');
+        newTimeText.innerText = text;
+        newTimeText.setAttribute('id', 'time-text');
+        timeDisplay.appendChild(newTimeText);
+    }
+    event.preventDefault();
+});
 
 
 
