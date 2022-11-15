85e70176a2514171b792d5b2fb47c1be85034acd
twang72 <71084257+twang72@users.noreply.github.com>
2022-11-14 23:34:41 -0800

    Week 10 in class activity.

--- week9/in-class-activity/src/components/AddTask.js
+++ week9/in-class-activity/src/components/AddTask.js
@@ -1,11 +1,27 @@
 import React, {useState} from 'react'
 
-export default function Form() {
+export default function AddTask() {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("submitted", { title: title, date: date });
+    try {
+      const response = await fetch ("http://localhost:5002/tasks", {
+        method: "POST",
+        headers: { 'content-type': "application/json"},
+        body: JSON.stringify({title: title, date: date}),
+      });
+      if (!response.ok) {
+        throw new Error(`Http error! Status: ${response.status} `);
+      }
+    } catch (err){
+      console.log ('post', err);
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="form-control">
       <label>Title</label>
       <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} />
--- week9/in-class-activity/src/components/TasksList.js
+++ week9/in-class-activity/src/components/TasksList.js
@@ -1,36 +1,52 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Task from './Task';
 
 export default function TaskLists() {
+    const [tasks, setTasks] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                const response = await fetch("http://localhost:5002/tasks");
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                } 
+                const data = await response.json();
+                setTasks(data);
+                setIsLoading(false);
+            } catch (err) {
+                console.log("fetch data", err);
+            }
+        };
+        fetchData();
+    }, []);
 
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            title: 'Review week 9 material',
-            date: 'June 4th at 1 pm',
-        },
-        {
-            id: 2,
-            title: 'Do quiz 9',
-            date: 'June 4th at 6 pm',
-        },
-        {
-            id: 3,
-            title: 'Work on assignment 2',
-            date: 'June 5th at 8 am',
-        },
-    ]);
-
-    const deletePressed = (deletedId) => {
-        console.log('clicked', deletedId);
-        const result = tasks.filter((item) => { return item.id !== deletedId; });
+    const deletePressed = async (deletedId) => {
+        //console.log('clicked', deletedId);
+        //const result = tasks.filter((item) => { return item.id !== deletedId; });
+        //setTasks(result);
+        try {
+            const response =  await fetch(`http://localhost:5002/tasks/${deletedId}`, {method:"DELETE",});
+            if (!response.ok) {
+                throw new Error(`Http error! Status: ${response.status} `);
+            }
+        } catch (err) {
+            console.log("delete", err);
+        }
+        const result = tasks.filter((item) => {
+            return item.id !== deletedId;
+        });
         setTasks(result);
     };
 
-    return(
-        tasks.length!== 0 ?
-        tasks.map((item) => <Task key={item.id} task={item} deleteHandler={deletePressed}/>)
-        :
-        <li> No Tasks Left </li>
-    )
+   if (isLoading) {
+    return <p>Loading</p>
+   }
+   return tasks.length === 0 ? (
+    <li>No task left!</li>
+   ) : (
+    tasks.map((item) => (
+        <Task key={item.id} task={item} deleteHandler={deletePressed} />
+    ))
+    );
 }
