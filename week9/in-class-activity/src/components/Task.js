16c6d827600e7269cf0fea65a7b61b0ab605a106
twang72 <71084257+twang72@users.noreply.github.com>
2022-11-28 16:45:33 -0800

    Update the import in Task.js.

--- week9/in-class-activity/src/components/Task.js
+++ week9/in-class-activity/src/components/Task.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FaTimes } from 'react-icons/fa';
-import { Link } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 export default function Task({ task, deleteHandler }) {
   return (
