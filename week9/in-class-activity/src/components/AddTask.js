dbd16af528ef5472e96c25d0aa102d80f6a0e7a6
twang72 <71084257+twang72@users.noreply.github.com>
2022-11-28 16:39:37 -0800

    Finish the in-class activity for week 11.

--- week9/in-class-activity/src/components/AddTask.js
+++ week9/in-class-activity/src/components/AddTask.js
@@ -1,40 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function AddTask() {
-  const [title, setTitle] = useState("");
-  const [date, setDate] = useState("");
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
   let navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("submitted", { title: title, date: date });
+    console.log('submitted', { title: title, date: date });
     try {
-      const response = await fetch ("http://localhost:5002/tasks", {
-        method: "POST",
-        headers: { 'content-type': "application/json"},
-        body: JSON.stringify({title: title, date: date}),
+      const response = await fetch('http://localhost:5002/tasks', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ title: title, date: date }),
       });
       if (!response.ok) {
         throw new Error(`Http error! Status: ${response.status} `);
       }
-      navigate("/tasks");
-    } catch (err){
-      console.log ('post', err);
+      navigate('/tasks');
+    } catch (err) {
+      console.log('post', err);
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="form-control">
-      <label>Title</label>
-      <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} />
+        <label>Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <div className="form-control">
-      <label>Date</label>
-      <input type="text" value={date} onChange={(e)=>setDate(e.target.value)}></input>
+        <label>Date</label>
+        <input
+          type="text"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        ></input>
       </div>
-      <input type="submit" value="Save Task"/>
+      <input type="submit" value="Save Task" />
     </form>
-  )
+  );
 }
--- week9/in-class-activity/src/components/Task.js
+++ week9/in-class-activity/src/components/Task.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { Link } from 'react-icons/fa';
 
-export default function Task ({task, deleteHandler}) {
-    return (
-        <li>
-            <div className='taskContainer'>
-                <div className='iconNameContainer'>
-                    <p>
-                        <Link to={`/tasks/${task.id}`}></Link>
-                    </p>
-                    <p>{task.title}</p>
-                    <FaTimes onClick = {() => deleteHandler(task.id)}
-                     />
-                </div>
-                <div>
-                    <p>{task.date}</p>
-                </div>
-            </div>
-        </li>
-    );
+export default function Task({ task, deleteHandler }) {
+  return (
+    <li>
+      <div className="taskContainer">
+        <div className="iconNameContainer">
+          <p>
+            <Link to={`/tasks/${task.id}`}>{task.title}</Link>
+          </p>
+          <FaTimes onClick={() => deleteHandler(task.id)} />
+        </div>
+        <div>
+          <p>{task.date}</p>
+        </div>
+      </div>
+    </li>
+  );
 }
